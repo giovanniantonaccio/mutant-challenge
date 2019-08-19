d8d4e01d2f20e5e68d1b3e9f6cbd531f574b85c4
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-08-19 15:24:36 -0400

    add tests with jest

--- src/app/middlewares/users.js
+++ src/app/middlewares/users.js
@@ -3,7 +3,6 @@ import api from '../../lib/api';
 export default async (req, res, next) => {
   try {
     const response = await api.get('/users');
-
     req.users = response.data;
     return next();
   } catch (err) {
